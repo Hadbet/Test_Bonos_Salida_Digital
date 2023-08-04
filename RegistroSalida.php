777bb276bff37680b594591eb1bd3ed7e093202c
hadbet <hadbet.altamirano@grammer.com>
2023-08-04 10:51:22 -0600

    Correccion de estructura

--- RegistroSalida.php
+++ RegistroSalida.php
@@ -179,32 +179,34 @@
 
                     <div id="fila" style="">
                         <hr>
-                        <div class="col-12 col-12-small">
-                            <p style="margin-bottom: 10px !important;">Descripción / Modelo / No.Serie</p>
-                            <input type="text" placeholder="Descripción" name="Descripcion" id="txtDescripcion"
-                            />
-                        </div>
+                        <div id="row">
+                            <div class="col-12 col-12-small">
+                                <p style="margin-bottom: 10px !important;">Descripción / Modelo / No.Serie</p>
+                                <input type="text" placeholder="Descripción" name="Descripcion" id="txtDescripcion"
+                                />
+                            </div>
 
-                        <div class="col-4 col-12-small" style="float: left; width: 50%;padding-right: 20px">
-                            <p style="margin-bottom: 10px !important; margin-top: 10px;">Cantidad</p>
-                            <input type="number" placeholder="Cantidad" name="Cantidad" id="txtCantidad" min="1"
-                                   pattern="^[0-9]+"/>
-                        </div>
+                            <div class="col-4 col-12-small">
+                                <p style="margin-bottom: 10px !important; margin-top: 10px;">Cantidad</p>
+                                <input type="number" placeholder="Cantidad" name="Cantidad" id="txtCantidad" min="1"
+                                       pattern="^[0-9]+"/>
+                            </div>
 
-                        <div class="col-4 col-12-small" style="float: right; width: 50%;padding-left: 20px">
-                            <p style="margin-bottom: 10px !important; margin-top: 10px;">U/M</p>
-                            <select style="margin-bottom: 20px" placeholder="Unidad de medida" name="um" id="txtUm">
-                                <option value="" selected>Seleccione la unidad de medida</option>
-                                <option value="Pieza">Piezas</option>
-                                <option value="Kilogramos">Kilogramos</option>
-                                <option value="Litros">Litros</option>
-                                <option value="Libras">Libras</option>
-                                <option value="Centimetros">Centímetros</option>
-                                <option value="Metros">Metros</option>
-                                <option value="Gramos">Gramos</option>
-                                <option value="Onzas">Onzas</option>
-                                <option value="Toneladas">Toneladas</option>
-                            </select>
+                            <div class="col-4 col-12-small">
+                                <p style="margin-bottom: 10px !important; margin-top: 10px;">U/M</p>
+                                <select style="margin-bottom: 20px" placeholder="Unidad de medida" name="um" id="txtUm">
+                                    <option value="" selected>Seleccione la unidad de medida</option>
+                                    <option value="Pieza">Piezas</option>
+                                    <option value="Kilogramos">Kilogramos</option>
+                                    <option value="Litros">Litros</option>
+                                    <option value="Libras">Libras</option>
+                                    <option value="Centimetros">Centímetros</option>
+                                    <option value="Metros">Metros</option>
+                                    <option value="Gramos">Gramos</option>
+                                    <option value="Onzas">Onzas</option>
+                                    <option value="Toneladas">Toneladas</option>
+                                </select>
+                            </div>
                         </div>
 
                         <div class="col-4 col-12-small">
