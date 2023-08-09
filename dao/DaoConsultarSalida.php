4d73051306f9c38dbae22f78cb6629803eefd8b5
hadbet <hadbet.altamirano@grammer.com>
2023-08-08 18:12:21 -0600

    Consulta y vista de los bonos multiples

--- dao/DaoConsultarSalida.php
+++ dao/DaoConsultarSalida.php
@@ -13,10 +13,12 @@ function Contador($Id){
     if (substr($Id, 0, 1) === "M") {
         $datos = mysqli_query($conex, "SELECT * FROM `BitacoraBonosSalidaMultiples` WHERE `PDF` = '$Id';");
         $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        echo "SELECT * FROM `BitacoraBonosSalidaMultiples` WHERE `PDF` = '$Id';";
         echo json_encode(array("data"=>$resultado));
     } else {
         $datos = mysqli_query($conex, "SELECT * FROM `BitacoraBonosSalida` WHERE `ImagenRegistro` = '$Id';");
         $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        echo "SELECT * FROM `BitacoraBonosSalida` WHERE `ImagenRegistro` = '$Id';";
         echo json_encode(array("data"=>$resultado));
     }
 
